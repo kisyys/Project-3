ca61f6720440766b5147d939ff2c52f28e910d25
Kimmo S <kisyys22@gmail.com>
2022-11-17 22:54:52 +0200

    Updates

--- Project 2/script.js
+++ Project 2/script.js
@@ -8,21 +8,31 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
-backgroundLayer.src="layer-1.png";
+backgroundLayer1.src="layer-1.png";
 const backgroundLayer2 = new Image();
-backgroundLayer.src="layer-2.png";
+backgroundLayer2.src="layer-2.png";
 const backgroundLayer3 = new Image();
-backgroundLayer.src="layer-3.png";
+backgroundLayer3.src="layer-3.png";
 const backgroundLayer4 = new Image();
-backgroundLayer.src="layer-4.png";
+backgroundLayer4.src="layer-4.png";
 const backgroundLayer5 = new Image();
-backgroundLayer.src="layer-5.png";
+backgroundLayer5.src="layer-5.png";
+
+let x = 0;
+let x2 = 2400;
 
 animate = () => {
-    ctx.drawImage(backgroundLayer4, 0, 0);   
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer4, x, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+    if(x<-2400) x = 2400 + x2 - gameSpeed;
+    else x -=gameSpeed;
+    if(x2<-2400) x2 = 2400 + x - gameSpeed;
+    else x2 -=gameSpeed;  
+    requestAnimationFrame(animate);   
 }
 animate();
 
@@ -93,5 +103,5 @@ animate();
     
 
 //     gameFrame++;
-//     requestAnimationFrame(animate);
+//     
 // }
